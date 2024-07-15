f00973b80947e922d70ebbe2fd73418a9fe76a91
Issy Long <me@issylong.com>
2024-07-15 16:13:07 -0400

    allure: Fix `Homebrew/NoFileutilsRmrf` offenses

--- Formula/a/allure.rb
+++ Formula/a/allure.rb
@@ -18,7 +18,7 @@ class Allure < Formula
 
   def install
     # Remove all windows files
-    rm_f Dir["bin/*.bat"]
+    rm(Dir["bin/*.bat"])
 
     prefix.install_metafiles
     libexec.install Dir["*"]
